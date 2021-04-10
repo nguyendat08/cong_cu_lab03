01b03256fb89199d115d19aa01d270b23a397185
“nguyendat08git config --global user.email “nguyenhuudat2308@gmail.com <nguyenhuudat2308@gmail.com>
2021-04-10 10:57:14 +0700

    Them 1 class và push len server

--- Buoi04_CongCu/Buoi04_CongCu/Lab3Git.cs
+++ Buoi04_CongCu/Buoi04_CongCu/Lab3Git.cs
@@ -5,7 +5,11 @@ using System.Web;
 
 namespace Buoi04_CongCu
 {
+    //sửa 1 ít về file class này
     public class Lab3Git
     {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
     }
 }
